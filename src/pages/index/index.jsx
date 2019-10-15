84c473bc1488848c372237b8a6c58ef3a4dfdf38
Riven <4771215@qq.com>
2019-10-15 16:56:17 +0800

    main page render

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Image, Swiper, SwiperItem } from '@tarojs/components'
 import './index.scss'
 
 export default class Index extends Component {
@@ -8,6 +8,58 @@ export default class Index extends Component {
     navigationBarTitleText: '首页'
   }
 
+  constructor () {
+    super(...arguments)
+    this.state = {
+      pages: [
+        {
+          id: 'microbit',
+          title: 'Micro:bit',
+          content: 'Microbit 控制',
+        },
+        {
+          id: 'robotbit',
+          title: 'Robot:Bit',
+          content: 'Robotbit 遥控',
+        },
+        {
+          id: 'iot',
+          title: 'Iot',
+          content: 'Iot 面板',
+        },
+        {
+          id: 'nanobit',
+          title: 'Nano:Bit',
+          content: 'Nanobit Io 与oled',
+        },
+        {
+          id: 'microbit',
+          title: 'Micro:bit',
+          content: 'Microbit 控制',
+        },
+        {
+          id: 'robotbit',
+          title: 'Robot:Bit',
+          content: 'Robotbit 遥控',
+        },
+        {
+          id: 'iot',
+          title: 'Iot',
+          content: 'Iot 面板',
+        },
+        {
+          id: 'nanobit',
+          title: 'Nano:Bit',
+          content: 'Nanobit Io 与oled',
+        },
+      ],
+      imgUrls: [
+        'https://img10.360buyimg.com/babel/s700x360_jfs/t25855/203/725883724/96703/5a598a0f/5b7a22e1Nfd6ba344.jpg!q90!cc_350x180',
+        'https://img11.360buyimg.com/babel/s700x360_jfs/t1/4776/39/2280/143162/5b9642a5E83bcda10/d93064343eb12276.jpg!q90!cc_350x180',
+        'https://img14.360buyimg.com/babel/s700x360_jfs/t1/4099/12/2578/101668/5b971b4bE65ae279d/89dd1764797acfd9.jpg!q90!cc_350x180'
+      ],
+    }
+  }
   componentWillMount () { }
 
   componentDidMount () { }
@@ -20,8 +72,43 @@ export default class Index extends Component {
 
   render () {
     return (
-      <View className='index'>
-        <Text>Hello world!</Text>
+      <View className='page page-index'>
+        <View className='caro'>
+          <Swiper
+            indicatorColor='#999'
+            indicatorActiveColor='#333'
+            current={1}
+            duration={500}
+            interval={5000}
+            circular
+            autoplay
+            indicatorDots
+            preMargin='20'
+          >
+            {
+              this.state.imgUrls.map((item, idx) => (
+                <SwiperItem key={idx}>
+                  <Image mode='widthFix' src={item} className='slide-image' width='355' height='150' />
+                </SwiperItem>
+              ))
+            }
+          </Swiper>
+        </View>
+        <View className='module-list'>
+          {this.state.pages.map((item, index) => (
+            <View
+              className='module-list__item'
+              key={index}
+              data-id={item.id}
+              data-name={item.title}
+              data-list={item.subpages}
+              onClick={this.gotoPanel}
+            >
+              <View className='module-list__item-title'>{item.title}</View>
+              <View className='module-list__item-content'>{item.content}</View>
+            </View>
+          ))}
+        </View>
       </View>
     )
   }
